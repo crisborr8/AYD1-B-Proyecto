0bb1e1521e20831bfb6b3d93ff56dcdf3c01a517
Christofer <crisborr8@gmail.com>
2022-05-03 19:35:41 -0600

    [ADD]Pruebas funcionales y no funcionales

--- BlockTest/UnitTest1.cs
+++ BlockTest/UnitTest1.cs
@@ -7,10 +7,11 @@ using BlockBusted.Models;
 namespace BlockTest
 {
     [TestClass]
-    public class UnitTest1
+    public class FunctionalTest
     {
+        //Funcionales
         [TestMethod]
-        public void Christofer_loginTest()
+        public void Unit_loginTest()
         {
             BlockBusted.Models.Usuario usuario = new BlockBusted.Models.Usuario();
             usuario.email = "freddy@gmail.com";
@@ -18,13 +19,64 @@ namespace BlockTest
             Assert.IsNotNull(usuario.Login(usuario), "Usuario incorrecto");
         }
         [TestMethod]
-        public void Christofer_BadloginTest()
+        public void Unit_Transfer()
+        {
+            BlockBusted.Models.Pelicula mdl = new BlockBusted.Models.Pelicula();
+            int res = (int)mdl.transferir("freddy@gmail.com", "soluna@gmail.com", 1).Tables[0].Rows[0].ItemArray[0];
+            Assert.AreEqual(0, res, "Envio correcto");
+        }
+        [TestMethod]
+        public void Unit_Historial()
+        {
+            BlockBusted.Models.Pelicula mdl = new BlockBusted.Models.Pelicula();
+            int res = (int)mdl.getPeliculasHistorial("soluna@gmail.com").Tables[0].Rows.Count;
+            Assert.AreNotEqual(0, res, "Mostrando historial");
+        }
+        [TestMethod]
+        public void Unit_Registrar()
+        {
+            BlockBusted.Models.Pelicula mdl = new BlockBusted.Models.Pelicula();
+            int res = (int)mdl.insertarUsuario("email2@gmail.com", "test1", "1234", "Testist", "Testost", 33, "0000000"); ;
+            Assert.AreEqual(1, res, "Usuario registrado");
+        }
+
+
+    }
+
+    [TestClass]
+    public class NoFunctionalTest
+    {
+
+        //No funcionales
+        [TestMethod]
+        public void Unit_loginTest()
         {
             BlockBusted.Models.Usuario usuario = new BlockBusted.Models.Usuario();
             usuario.email = "freddy@gmail.com";
             usuario.clave = "12345xxxx";
             String res = usuario.Login(usuario).Tables[0].Rows[0].ItemArray[0] + "";
             Assert.AreSame("0", res, "Usuario incorrecto");
+        }
+        [TestMethod]
+        public void Unit_Transfer_NoMovie()
+        {
+            BlockBusted.Models.Pelicula mdl = new BlockBusted.Models.Pelicula();
+            int res = (int)mdl.transferir("coreo1", "correo2", 0).Tables[0].Rows[0].ItemArray[0];
+            Assert.AreEqual(1, res, "Pelicula no existe");
+        }
+        [TestMethod]
+        public void Unit_Transfer_NoEmail()
+        {
+            BlockBusted.Models.Pelicula mdl = new BlockBusted.Models.Pelicula();
+            int res = (int)mdl.transferir("coreo1", "correo2", 1).Tables[0].Rows[0].ItemArray[0];
+            Assert.AreEqual(1, res, "Correo de usuario inexistente");
+        }
+        [TestMethod]
+        public void Unit_Transfer_NoFriend()
+        {
+            BlockBusted.Models.Pelicula mdl = new BlockBusted.Models.Pelicula();
+            int res = (int)mdl.transferir("freddy@gmail.com", "correo2", 1).Tables[0].Rows[0].ItemArray[0];
+            Assert.AreEqual(1, res, "Correo de amigo inexistente");
         }
     }
 }
